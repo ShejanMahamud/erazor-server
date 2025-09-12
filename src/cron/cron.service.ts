2ab7f97784a7c941a835ff03d28ca36c8ed2047e
ShejanMahamud <dev.shejanmahamud@gmail.com>
2025-09-12 19:33:51 +0600

    feat: update CronService to schedule daily database backups at midnight with specified timezone

--- src/cron/cron.service.ts
+++ src/cron/cron.service.ts
@@ -5,7 +5,10 @@ import { backupDatabase } from 'src/backup';
 
 @Injectable()
 export class CronService {
-    @Cron(CronExpression.EVERY_30_SECONDS)
+    @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT, {
+        name: 'daily-db-backup',
+        timeZone: 'Asia/Dhaka'
+    })
     async DBBackup() {
         await backupDatabase();
     }
