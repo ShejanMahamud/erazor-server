235cf5511782509a259963d7d2df307c21839e25
ShejanMahamud <dev.shejanmahamud@gmail.com>
2025-10-04 19:45:51 +0600

    added new way in sse endpoint

--- src/images/events/image-events.controller.ts
+++ src/images/events/image-events.controller.ts
@@ -2,10 +2,11 @@ import {
     Controller,
     MessageEvent,
     Req,
+    Res,
     Sse,
     UseGuards
 } from '@nestjs/common';
-import type { Request } from 'express';
+import type { Request, Response } from 'express';
 import { Observable } from 'rxjs';
 import { OptionalClerkGuard } from 'src/guards/optional-clerk.guard';
 import { ImageEventsService } from './image-events.service';
@@ -16,7 +17,11 @@ export class ImageEventsController {
 
     @UseGuards(OptionalClerkGuard)
     @Sse('events')
-    events(@Req() req: Request): Observable<MessageEvent> {
+    events(@Req() req: Request, @Res({ passthrough: true }) res: Response): Observable<MessageEvent> {
+        res.setHeader('Access-Control-Allow-Origin', 'https://www.erazor.app');
+        res.setHeader('Access-Control-Allow-Credentials', 'true');
+        res.setHeader('Cache-Control', 'no-cache');
+        res.setHeader('Connection', 'keep-alive');
         return this.imageEventsService.subscribeToUser(req.user?.sub);
     }
 }
