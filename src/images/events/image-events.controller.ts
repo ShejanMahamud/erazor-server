452c1b5169d0e754aac2c2361161307e96ad80ab
ShejanMahamud <dev.shejanmahamud@gmail.com>
2025-10-04 16:07:03 +0600

    fix: replace ClerkGuard with OptionalClerkGuard in ImageEventsController for improved access control

--- src/images/events/image-events.controller.ts
+++ src/images/events/image-events.controller.ts
@@ -6,14 +6,14 @@ import {
     UseGuards
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
-import { ClerkGuard } from 'src/guards/clerk.guard';
+import { OptionalClerkGuard } from 'src/guards/optional-clerk.guard';
 import { ImageEventsService } from './image-events.service';
 
 @Controller('images')
 export class ImageEventsController {
     constructor(private readonly imageEventsService: ImageEventsService) { }
 
-    @UseGuards(ClerkGuard)
+    @UseGuards(OptionalClerkGuard)
     @Sse('events/:userId')
     events(@Param('userId') userId: string): Observable<MessageEvent> {
         return this.imageEventsService.subscribeToUser(userId);
