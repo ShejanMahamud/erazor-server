d7144df559edae591fecf32885543112cf235b1a
ShejanMahamud <dev.shejanmahamud@gmail.com>
2025-10-05 16:44:16 +0600

    refactor: remove unused ClerkGuard and clean up imports in ImageEventsController

--- src/images/events/image-events.controller.ts
+++ src/images/events/image-events.controller.ts
@@ -2,18 +2,15 @@ import {
     Controller,
     MessageEvent,
     Param,
-    Sse,
-    UseGuards
+    Sse
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
-import { ClerkGuard } from 'src/guards/clerk.guard';
 import { ImageEventsService } from './image-events.service';
 
 @Controller('images')
 export class ImageEventsController {
     constructor(private readonly imageEventsService: ImageEventsService) { }
 
-    @UseGuards(ClerkGuard)
     @Sse('events/:userId')
     events(@Param('userId') userId: string): Observable<MessageEvent> {
         return this.imageEventsService.subscribeToUser(userId);
